3950ac9c6f502e30bb91907973da3d8d424e8ac1
Mike <ogar.m.v@mail.ru>
2024-05-21 11:47:35 +0300

    Hibernate Skillbox courses

---
+++ src/main/java/Main.java
@@ -0,0 +1,21 @@
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.metamodel.Metadata;
+import org.hibernate.metamodel.MetadataSources;
+
+public class Main {
+    public static void main(String[] args) {
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure("hibernate.cfg.xml").build();
+        Metadata metadata = new MetadataSources(registry).getMetadataBuilder().build();
+        SessionFactory sessionFactory = metadata.getSessionFactoryBuilder().build();
+
+        Session session = sessionFactory.openSession();
+        Course course = (Course) session.get(Course.class, 46);
+        System.out.println(course.getName());
+
+        sessionFactory.close();
+    }
+}
