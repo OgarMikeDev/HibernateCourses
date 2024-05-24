0769a93dc596e45f6542f06fd342a3e83b260dc9
ogar_m <ogar.m.v@mail.ru>
2024-05-24 10:31:29 +0300

    hibernate

---
+++ src/main/java/MainTwo.java
@@ -0,0 +1,30 @@
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import java.util.List;
+
+public class MainTwo {
+    public static void main(String[] args) {
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure("hibernate.cfg.xml").build();
+        Metadata metadata = new MetadataSources(registry)
+                .getMetadataBuilder().build();
+        SessionFactory sessionFactory = metadata.getSessionFactoryBuilder().build();
+
+        Session session = sessionFactory.openSession();
+
+        String hql = "From " + Course.class.getName() + " WHERE price >= 100000";
+        List<Course> courses = session.createQuery(hql).setMaxResults(5).getResultList();
+        courses.forEach(elem -> System.out.println(elem.getPrice()));
+
+        sessionFactory.close();
+    }
+}
