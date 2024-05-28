1b43f96382e68d5b59c7e5d64fda13355b3433cb
OgarMikeDev <123175953+OgarMikeDev@users.noreply.github.com>
2024-05-28 10:06:56 +0300

    Update MainTwo.java

--- src/main/java/MainTwo.java
+++ src/main/java/MainTwo.java
@@ -1,3 +1,4 @@
+import org.hibernate.query.Query;
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
 import jakarta.persistence.criteria.Root;
@@ -21,10 +22,29 @@ public class MainTwo {
 
         Session session = sessionFactory.openSession();
 
-        String hql = "From " + Course.class.getName() + " WHERE price >= 100000 ORDER BY price desc LIMIT 5";
+//        String hql = "From " + Course.class.getName() + " WHERE price >= 100000 ORDER BY price desc";
 //        List<Course> courses = session.createQuery(hql).setMaxResults(5).getResultList();
-        List<Course> courses = session.createQuery(hql).getResultList();
-        courses.forEach(elem -> System.out.println(elem.getPrice()));
+//        courses.forEach(elem -> System.out.println(elem.getPrice()));
+
+//        String hql = "SELECT count(course_id)/(SELECT MAX(MONTH(subscription_date)) " +
+//                "FROM " + Subscription.class.getName() + ") as count, " +
+//                "course_id FROM " + Subscription.class.getName() + " GROUP BY course_id";
+
+        String hql = "SELECT s.courseId, COUNT(*) AS totalSubscriptions\n" +
+                "FROM Subscription s\n" +
+                "GROUP BY s.courseId\n" +
+                "ORDER BY totalSubscriptions DESC";
+
+        Query query = session.createQuery(hql, Subscription[].class);
+        List<String[]> resultList = query.getResultList();
+
+        for (Object[] row : resultList) {
+//            int courseId = Integer.parseInt(row[0]);
+//            float totalSubscriptions = Float.parseFloat(row[1]);
+            int courseId = (int) row[0];
+            float totalSubscriptions = (long) row[1];
+            System.out.println("Course ID: " + courseId + ", Total Subscriptions: " + totalSubscriptions / 9);
+        }
 
         sessionFactory.close();
     }
